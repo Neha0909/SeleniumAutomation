be22788f22aac50d8972e903200b2d8ceb5bc3d0
Neha <Neha@192.168.2.8>
2017-05-14 02:45:57 +0530

    //First Commit for selenium automation

---
+++ SeleniumWebdriver/src/com/testcases/PrintSelenium.java
@@ -0,0 +1,11 @@
+package com.testcases;
+
+public class PrintSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Print Selenium");
+
+	}
+
+}
